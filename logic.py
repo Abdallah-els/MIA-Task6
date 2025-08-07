677d353c0d1d2bbecd918ace316351301bc7314a
AbdallahElsherbini <body01022@gmail.com>
2025-08-07 20:30:48 +0300

    add colored answers

--- logic.py
+++ logic.py
@@ -1,37 +1,47 @@
 from Backend import *
 
 def user_guess(): # takes the guesses
+
     guess = input("")
     return guess
 
 def check_guess(guess): # checks if the user won
+
     if guess == answer():
         print("you got it!")
     else:
         check_matches(guess)
 
+
 def check_matches(guess): 
+
     ans = answer()
     exact_matches = ""
-    worng_matches = []
-    wrong_position = ""
+    worng_matches = {}
+    colors = ['\033[0m' , '\033[0m' , '\033[0m' , '\033[0m' ,'\033[0m'] # default color all letters white
 
     for i in range(5): # checks the letters 1 by 1 to see which letter is in its exact position
         if guess[i] == ans[i]:
             exact_matches += guess[i]
+            colors[i] = '\033[32m' # change color of correct letters to green
         else :
             exact_matches += "_"
-            worng_matches.append(guess[i])
+            worng_matches[i] = guess[i] # key: index, value: letter
     
-    for letter in worng_matches: #checks correct letters in wrong position
+    for key , value in worng_matches.items(): #checks correct letters in wrong position
         for i in range(5):
-            if letter == ans[i] and letter != exact_matches[i]:
-                wrong_position += letter
-                wrong_position += " "
+            if value == ans[i] and value != exact_matches[i]:
+                colors[key] = '\033[33m'
                 ans = ans[:i] + "-" + ans[i+1:] # removing checked letter to prevent letter repeating bug
 
+    color_answer(guess,colors)
 
-    print(exact_matches + " - correct letters but wrong position: " + wrong_position)
 
-    
+def color_answer(guess,colors): # combine the guess with the colors
 
+    colored_word = ""
+    for i, char in enumerate(guess):
+        colored_word += colors[i] + char
+    colored_word += '\033[0m'
+
+    print (colored_word)
