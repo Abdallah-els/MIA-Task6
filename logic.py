966ea93a1213b379289f9cd48b1dea68949e7f88
AbdallahElsherbini <body01022@gmail.com>
2025-08-07 19:34:35 +0300

    finish letters matches

--- logic.py
+++ logic.py
@@ -8,13 +8,28 @@ def check_guess(guess): # checks if the user won
     if guess == answer():
         print("you got it!")
     else:
-        check_exact_matches(guess)
+        check_matches(guess)
 
-def check_exact_matches(guess):
+def check_matches(guess): 
     exact_matches = ""
-    for i in range(5):
+    worng_matches = []
+    wrong_position = ""
+
+    for i in range(5): # checks the letters 1 by 1 to see which letter is in its exact position
         if guess[i] == answer()[i]:
             exact_matches += guess[i]
         else :
             exact_matches += "_"
-    print(exact_matches)
+            worng_matches.append(guess[i])
+    
+    for letter in worng_matches: #checks correct letters in wrong position
+        for i in range(5):
+            if letter == answer()[i] and letter != exact_matches[i]:
+                wrong_position += letter
+                wrong_position += " - "
+
+
+    print(exact_matches + " - correct letters but wrong position: " + wrong_position)
+
+    
+
