96d6dda6d232c233ff2cba2ec714ab157ac4476d
AbdallahElsherbini <body01022@gmail.com>
2025-08-07 18:14:24 +0300

    add choosing random word as an answer

--- Backend.py
+++ Backend.py
@@ -1,5 +1,10 @@
+import random
+
 def read_database():
     with open('words.txt', 'r') as database:
-        words =[word.strip() for word in database.readlines()]
-        
+        words =[word.strip() for word in database.readlines()] # makes a list of all words with out the /n
+    return words
+            
+def choose_answer():
+    return random.choice(read_database()) #choose random word every time
 
