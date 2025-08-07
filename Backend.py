66be8f8f531e9fa28d409be0bec70b9aeb4b9057
AbdallahElsherbini <body01022@gmail.com>
2025-08-07 18:24:09 +0300

    validate user guess

--- Backend.py
+++ Backend.py
@@ -8,3 +8,10 @@ def read_database():
 def choose_answer():
     return random.choice(read_database()) #choose random word every time
 
+def validating_user_word(guess): #return 0 when guess is correct and 1 when incorrect
+    if guess in read_database():
+        return 0
+    else:
+        return 1
+
+
